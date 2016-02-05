4f4e52d21f38db131581aa628e9c0ae16b98a25f
Reidar Djupvik <mail@3rdhand.info>
2016-02-05 13:33:52 +0000

    Resolve db path with node.js path module. Log db destruction result to console.

--- bin/drop-db.js
+++ bin/drop-db.js
@@ -1,5 +1,7 @@
 #! /usr/bin/env node
 
+var path = require('path');
 var PouchDB = require('pouchdb');
-var db = new PouchDB('./db/thirdhandinfo');
-db.destroy();
+var db = new PouchDB(path.resolve(__dirname, '../db/thirdhandinfo'));
+var log = console.log.bind(console);
+db.destroy().then(log).catch(log);
--- bin/insert-ddoc.js
+++ bin/insert-ddoc.js
@@ -1,11 +1,12 @@
 #! /usr/bin/env node
 
+var path = require('path');
 var PouchDB = require('pouchdb');
 PouchDB.plugin(require('pouchdb-upsert'));
 
 var ddoc = require('../lib/ddoc.js');
 
-var db = new PouchDB('./db/thirdhandinfo');
+var db = new PouchDB(path.resolve(__dirname, '../db/thirdhandinfo'));
 
 db.upsert(ddoc._id, function () { return ddoc; }).then(function (res) {
   if (res.updated) {
