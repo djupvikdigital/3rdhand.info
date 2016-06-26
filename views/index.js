58171b89aa8b78b55e8f25f4c2d60fc8c47914c3
Reidar Djupvik <mail@3rdhand.info>
2016-06-26 13:34:03 +0200

    Rewrote IndexTemplate in JS.

---
+++ views/index.js
@@ -0,0 +1,50 @@
+const YAML = require('js-yaml');
+
+const { read } = require('../lib/utils.js');
+const Elements = require('../src/scripts/elements.coffee');
+
+const conf = YAML.safeLoad(read('./config.yaml'));
+
+const { assetServer } = conf;
+const server = assetServer ? `//${assetServer.hostname}:${assetServer.port}` : '';
+
+const assetPaths = conf.assetPaths || require('../dist/webpack-assets.json');
+
+const { html, head, meta, title, link, body, header, div, script } = Elements;
+
+const googleFontsUrl = '//fonts.googleapis.com/css?family=';
+const stylesheets = [
+  `${googleFontsUrl}Open+Sans:400,400italic,700,700italic,800italic`,
+  `${googleFontsUrl}Rokkitt:400,700`,
+  server + assetPaths.styles.css,
+];
+
+function IndexTemplate(props) {
+  const metadata = [
+    meta({ charSet: 'utf-8' }),
+    meta({ name: 'viewport', content: 'width=device-width, initial-scale=1' }),
+    props.title,
+    link({
+      rel: 'alternate',
+      type: 'application/atom+xml',
+      title: props.siteTitle,
+      href: '/index.atom',
+    }),
+  ].concat(props.meta, stylesheets.map(
+    href => link({ rel: 'stylesheet', href: href })
+  ));
+  return html(
+    { lang: props.lang },
+    head(metadata),
+    body(
+      div({ id: 'app', innerHtml: props.app }),
+      script(
+        { id: 'state', type: 'application/json' },
+        JSON.stringify(props.state)
+      ),
+      script({ src: server + assetPaths.scripts.js })
+    )
+  );
+}
+
+module.exports = IndexTemplate;
