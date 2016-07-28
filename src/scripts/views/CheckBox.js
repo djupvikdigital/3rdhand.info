efc4148a955aad7c8fd1c213e4436f075945ebdc
Reidar Djupvik <mail@3rdhand.info>
2016-07-28 15:44:17 +0100

    Make CheckBox remember value, setting it if checked

--- src/scripts/views/CheckBox.js
+++ src/scripts/views/CheckBox.js
@@ -1,14 +1,37 @@
+const { createClass } = require('react');
 const { elements } = require('react-elementary').default;
 
 const { input, label } = elements;
 
-function CheckBox(props) {
-  const { name, onChange, value } = props;
-  return label(
-    { className: 'form-group' },
-    `${props.label}: `,
-    input({ type: 'checkbox', name, onChange, value })
-  );
-}
+const CheckBox = createClass({
+  propsToState: function propsToState({ value }) {
+    return { value };
+  },
+  getInitialState: function getInitialState() {
+    return this.propsToState(this.props);
+  },
+  componentWillReceiveProps: function componentWillReceiveProps({ value }) {
+    if (value) {
+      this.setState({ value });
+    }
+  },
+  handleChange: function handleChange({ target }) {
+    const { name, onChange } = this.props;
+    const value = target.checked ? this.state.value : '';
+    if (typeof onChange == 'function') {
+      onChange({ target: { name, value } });
+    }
+  },
+  render: function render() {
+    const props = this.props;
+    const { name } = props;
+    const { value } = this.state;
+    return label(
+      { className: 'form-group' },
+      `${props.label}: `,
+      input({ type: 'checkbox', onChange: this.handleChange, name, value })
+    );
+  },
+});
 
 module.exports = CheckBox;
